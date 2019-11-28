f44f473736dc11cb82ae6613989e389f035e2983
quarkov <3dquarkov@gmail.com>
2019-11-28 22:15:05 +0100

    ready to release to polygon

--- verification/tests.py
+++ verification/tests.py
@@ -35,7 +35,7 @@ TESTS = {
             "answer": 2
         },
         {
-            "input": [({1: "one"}, {2: "two"}, "two", ["two", {"two": "one"}]), "two"],
+            "input": [[{1: "one"}, {2: "two"}, "two", ["two", {"two": "one"}]], "two"],
             "answer": 3
         },
     ],
@@ -44,5 +44,68 @@ TESTS = {
             "input": [[], []],
             "answer": 1
         },
+        {
+            "input": [
+                [{
+                    "one": [["checkio"], "checkio", {"checkio": 4, "py": 5}],
+                    "two": ["aaa", "bbb", "check", {"six": 6, "seven": 7}],
+                    "three": ["checkio", {"checkio": "checkio", 1: "checkio"}, {"8": "checkio", "9": 9}]
+                }], "checkio"],
+            "answer": 6
+        },
+        {
+            "input": [
+                [[{
+                    "one": [["checkio"], "checkio", {"checkio": 4, "py": 5}],
+                    "two": ["aaa", "bbb", "check", {"six": 6, "seven": 7}],
+                    "three": ["checkio", {"checkio": "checkio", 1: "checkio"}, {"8": "checkio", "9": 9}]
+                }]], "checkio"],
+            "answer": 6
+        },
+        {
+            "input": [
+                [[[[[[[[[[{
+                    "one": [["checkio"], "checkio", {"checkio": 4, "py": 5}],
+                    "two": ["aaa", "bbb", "check", {"six": 6, "seven": 7}],
+                    "three": ["checkio", {"checkio": "checkio", 1: "checkio"}, {"8": "checkio", "9": 9}]
+                }]]]]]]]]]], "checkio"],
+            "answer": 6
+        },
+        {
+            "input": [dict(), {}],
+            "answer": 1
+        },
+        {
+            "input": [[1, [2, [3, [4, [5, [6, [7, [8, [9, [1]]]]]]]]]], 1],
+            "answer": 2
+        },
+        {
+            "input": [[1, [2, [3, [4, [5, [6, [7, [8, [9, [1]]]]]]]]]], [9, [1]]],
+            "answer": 1
+        },
+        {
+            "input": [[1, [2, [3, [4, [5, [6, [7, [8, [9, [1]]]]]]]]]], [1, [2]]],
+            "answer": 0
+        },
+        {
+            "input": [[1, [2, [3, [4]]]], [1, [1, [1, [1]]]]],
+            "answer": 0
+        },
+        {
+            "input": [[1, [1, [1, [1]]]], [1, [1, [1, [1]]]]],
+            "answer": 1
+        },
+        {
+            "input": [{1: {2: {3: {5: {6: {7: {8: {9: 10}}}}}}}}, 10],
+            "answer": 1
+        },
+        {
+            "input": [{1: {2: {3: {5: {6: {7: {8: {9: 10}}}}}}}}, 1],
+            "answer": 0
+        },
+        {
+            "input": [dict(dict(dict(dict(dict(dict(dict())))))), dict()],
+            "answer": 1
+        }
     ]
 }
